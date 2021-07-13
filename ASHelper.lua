f1d83d09019c3776526cb50fd7f49f984870687f
Nazar1ky <78618525+Nazar1ky@users.noreply.github.com>
2021-07-13 11:49:52 +0300

    Update ASHelper.lua

--- ASHelper.lua
+++ ASHelper.lua
@@ -1,6 +1,6 @@
 require "lib.moonloader"
 
-local dlstatus require('moonloader').download_status
+local dlstatus = require('moonloader').download_status
 local se = require 'samp.events'
 local config = require "config"
 local res_imgui, imgui = pcall(require, 'imgui')
@@ -9,13 +9,13 @@ local encoding = require 'encoding'
 local inicfg = require 'inicfg'
 
 update_state = false
-local script_vers = 4
-local script_vers_text = "1.2"
+local script_vers = 3
+local script_vers_text = "1.1"
 
 local update_url = "https://raw.githubusercontent.com/Nazar1ky/ASHelper/main/update.ini"
 local update_path = getWorkingDirectory() .. "/update.ini"
 
-local script_url = ""
+local script_url = "https://github.com/Nazar1ky/ASHelper/raw/main/ASHelper.lua"
 local script_path = thisScript().path
 
 encoding.default = 'CP1251'
@@ -161,7 +161,7 @@ function main()
     downloadUrlToFile(update_url, update_path, function(id, status)
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load(nil, update_path)
-            tonumber(updateIni.info.vers) > script_vers then
+            if tonumber(updateIni.info.vers) > script_vers then
                 sampAddChatMessage(tag .. "Есть обновление! Версия: " .. updateIni.info.vers_text, 0xFFFF00)
                 update_state = true
             end
@@ -170,12 +170,9 @@ function main()
     end)
     while true do
         wait(0)
-
         if update_state then
-
-            downloadUrlToFile(update_url, update_path, function(id, status)
+            downloadUrlToFile(script_url, script_path, function(id, status)
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-
                 end
             end)
             break
